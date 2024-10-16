f9d2c417be14e4f2c9c18f52c3804264b4903881
Kyle Cardwell <kyle.e.cardwell@gmail.com>
2024-10-16 12:16:05 -0600

    fixed timeOff adjustments after builder modal updated

--- src/redux/actions/taskData.js
+++ src/redux/actions/taskData.js
@@ -1,56 +1,68 @@
 import { sortAndAdjustDates } from "../../utils/helpers";
 import { Actions } from "../actions";
-
+import { eachDayOfInterval } from "date-fns";
+import { normalizeDate } from "../../utils/dateUtils";
 export const updateTasksByOneBuilder = (builderId, taskList) => {
-  return {
-    type: Actions.taskData.UPDATE_TASKS_BY_ONE_BUILDER,
-    payload: { builderId, taskList },
-  };
+	return {
+		type: Actions.taskData.UPDATE_TASKS_BY_ONE_BUILDER,
+		payload: { builderId, taskList },
+	};
 };
 
 export const updateTasksAfterBuilderChanges = (
-  updatedBuilders,
-  deletedBuilderIds,
-  workdayHours,
-  holidayChecker,
-  holidays,
-  dayWidth,
-  chartStartDate
+	updatedBuilders,
+	deletedBuilderIds,
+	workdayHours,
+	holidayChecker,
+	holidays,
+	dayWidth,
+	chartStartDate
 ) => {
-  return (dispatch, getState) => {
-    const state = getState();
-    const allTasks = state.taskData.tasks;
+	return (dispatch, getState) => {
+		const state = getState();
+		const allTasks = state.taskData.tasks;
 
-    // Handle deleted builders
-    const tasksAfterDeletion = allTasks.map((task) =>
-      deletedBuilderIds.includes(task.builderId)
-        ? { ...task, builderId: "1" }
-        : task
-    );
+		// Create a complete timeOffByBuilder object
+		const timeOffByBuilder = updatedBuilders.reduce((acc, builder) => {
+			acc[builder.id] = builder.timeOff.flatMap((period) =>
+				eachDayOfInterval({
+					start: normalizeDate(new Date(period.start)),
+					end: normalizeDate(new Date(period.end)),
+				}).map((day) => normalizeDate(day).toISOString())
+			);
+			return acc;
+		}, {});
 
-    // Update tasks for each builder
-    const updatedTasks = updatedBuilders.reduce((acc, builder) => {
-      const builderTasks = acc.filter((task) => task.builderId === builder.id);
-      const sortedTasks = sortAndAdjustDates(
-        builderTasks,
-        workdayHours,
-        holidayChecker,
-        holidays,
-        builder.id,
-        null,
-        { [builder.id]: builder.timeOff },
-        dayWidth,
-        chartStartDate
-      );
-      return [
-        ...acc.filter((task) => task.builderId !== builder.id),
-        ...sortedTasks,
-      ];
-    }, tasksAfterDeletion);
+		// Handle deleted builders
+		const tasksAfterDeletion = allTasks.map((task) =>
+			deletedBuilderIds.includes(task.builderId)
+				? { ...task, builderId: "1" }
+				: task
+		);
 
-    dispatch({
-      type: Actions.taskData.UPDATE_TASKS_AFTER_BUILDER_CHANGES,
-      payload: updatedTasks,
-    });
-  };
+		// Update tasks for each builder
+		const updatedTasks = updatedBuilders.reduce((acc, builder) => {
+			const builderTasks = acc.filter((task) => task.builderId === builder.id);
+			const sortedTasks = sortAndAdjustDates(
+				builderTasks,
+				workdayHours,
+				holidayChecker,
+				holidays,
+				builder.id,
+				null,
+				timeOffByBuilder,
+				dayWidth,
+				chartStartDate
+			);
+			return [
+				...acc.filter((task) => task.builderId !== builder.id),
+				...sortedTasks,
+			];
+		}, tasksAfterDeletion);
+
+		dispatch({
+			type: Actions.taskData.UPDATE_TASKS_AFTER_BUILDER_CHANGES,
+			payload: updatedTasks,
+		});
+	};
 };
