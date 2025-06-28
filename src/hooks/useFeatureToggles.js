61a2d3071fa0a6d8ea7625e21f31e99291dc1c1d
Kyle Cardwell <kyle.e.cardwell@gmail.com>
2025-06-28 13:55:32 -0600

    added default route, update feature toggles hook for allowed teams

--- src/hooks/useFeatureToggles.js
+++ src/hooks/useFeatureToggles.js
@@ -2,8 +2,15 @@ import { useSelector } from 'react-redux';
 
 export const useFeatureToggles = () => {
   const { features } = useSelector((state) => state.featureToggles);
+  const { teamId } = useSelector((state) => state.auth);
 
-  return features;
+  // Transform features object to check both conditions
+  const transformedFeatures = Object.entries(features).reduce((acc, [featureName, { feature_enabled, enabled_teams }]) => ({
+    ...acc,
+    [featureName]: feature_enabled || (enabled_teams?.includes(teamId) ?? false)
+  }), {});
+
+  return transformedFeatures;
 };
 
 export default useFeatureToggles;
