682441c87b1285674704ea5cb4956b1b6d2fefe8
Kyle Cardwell <kyle.e.cardwell@gmail.com>
2025-09-24 11:18:39 -0600

    completed project container format as list

--- src/components/completedProjects/CompletedProjectCard.jsx
+++ src/components/completedProjects/CompletedProjectCard.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
-import { FiCheck, FiX } from "react-icons/fi";
+import { FiCheck, FiChevronDown, FiChevronUp, FiX } from "react-icons/fi";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -11,18 +11,19 @@ import {
   fetchTaskFinancials,
 } from "../../redux/actions/financialsData";
 
-// const categories = ["Busybusy", "Alpha", "Probox", "Doors", "Other"];
-
 const CompletedProjectCard = ({
   project,
   setIsFinancialsInputModalOpen,
   setSelectedTask,
+  bgColor,
 }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { canEditFinancials, canViewProfitLoss } = usePermissions();
   const [hoveredTaskId, setHoveredTaskId] = useState(null);
   const [hoveredProjectId, setHoveredProjectId] = useState(null);
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const jobName = project.project_name;
   const completedDate = new Date(
     project.project_completed_at
@@ -57,17 +58,18 @@ const CompletedProjectCard = ({
   };
 
   return (
-    <div
-      className="border border-gray-300 rounded-lg mb-5 p-4 shadow-sm bg-white"
-      onMouseEnter={() => setHoveredProjectId(project.project_id)}
-      onMouseLeave={() => setHoveredProjectId(null)}
-    >
-      <div className="flex justify-between items-center mb-4">
-        <div className="flex gap-4">
-          <h2 className="text-lg font-bold">{jobName}</h2>
+    <div className={`border-b border-gray-300 ${bgColor}`}>
+      <div
+        className="grid grid-cols-[1fr_1fr_1fr_1fr] items-center p-2"
+        onMouseEnter={() => setHoveredProjectId(project.project_id)}
+        onMouseLeave={() => setHoveredProjectId(null)}
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <div className="flex items-center gap-4">
+          <span className="ml-2 font-bold">{jobName}</span>
           <button
             onClick={handleViewClick}
-            className={`${buttonClass} bg-blue-500 ${
+            className={`px-3 py-1 text-sm font-medium text-white bg-blue-600 rounded hover:bg-blue-700 transition-opacity duration-200 ${
               hoveredProjectId === project.project_id
                 ? "opacity-100"
                 : "opacity-0"
@@ -76,65 +78,90 @@ const CompletedProjectCard = ({
             View
           </button>
         </div>
-        <span className="completed-date">Shop Completed: {completedDate}</span>
-      </div>
-      <div className="grid grid-cols-[100px_1fr_150px] gap-px bg-gray-200 border border-gray-200">
-        <div className="contents font-bold">
-          <span className="p-2 bg-gray-100">Job Number</span>
-          <span className="p-2 bg-gray-100">Room Name</span>
-          <span className="p-2 bg-gray-100">Costing {costingComplete ? "Complete" : "Incomplete"}</span>
+        <span>{completedDate}</span>
+        <div className="flex items-center justify-center gap-2">
+          {costingComplete ? (
+            <FiCheck className="text-green-500" />
+          ) : (
+            <FiX className="text-red-500" />
+          )}
+          <span>{costingComplete ? "Complete" : "Incomplete"}</span>
         </div>
-        {project.tasks.map((task, index) => {
-          const bgColor = index % 2 === 0 ? "bg-gray-50" : "bg-white";
-          return (
-            <div
-              key={task.task_id}
-              className="contents group"
-              onMouseEnter={() => setHoveredTaskId(task.task_id)}
-              onMouseLeave={() => setHoveredTaskId(null)}
-            >
-              <span className={`p-2 ${bgColor} group-hover:bg-blue-50`}>
-                {task.task_number}
-              </span>
-              <div className={`relative p-2 ${bgColor} group-hover:bg-blue-50`}>
-                <button
-                  onClick={() =>
-                    handleEditClick(
-                      task.task_id,
-                      task.task_name,
-                      task.task_number
-                    )
-                  }
-                  className={`absolute left-2 px-3 py-1 text-sm font-medium text-white bg-blue-600 rounded hover:bg-blue-700 transition-opacity duration-200 ${
-                    hoveredTaskId === task.task_id ? "opacity-100" : "opacity-0"
-                  } ${!canEditFinancials ? "hidden" : ""}`}
-                >
-                  Edit
-                </button>
-
-                <span>{task.task_name}</span>
-              </div>
-              <div
-                className={`relative p-2 ${bgColor} group-hover:bg-blue-50 flex justify-center items-center`}
-              >
-                {task.costing_complete ? (
-                  <FiCheck className="text-green-500" size={20} />
-                ) : (
-                  <FiX className="text-red-500" size={20} />
-                )}
-              </div>
-            </div>
-          );
-        })}
+        <div className="justify-self-end">
+          <button
+            // onClick={() => setIsExpanded(!isExpanded)}
+            className="justify-self-end p-2"
+          >
+            {isExpanded ? <FiChevronUp /> : <FiChevronDown />}
+          </button>
+        </div>
       </div>
+      {isExpanded && (
+        <div className="p-4 bg-gray-100">
+          <div className="grid grid-cols-[100px_1fr_150px] gap-px bg-gray-500 border border-gray-500">
+            <div className="contents font-bold text-center">
+              <span className="p-2 bg-gray-200">Job Number</span>
+              <span className="p-2 bg-gray-200">Room Name</span>
+              <span className="p-2 bg-gray-200">Costing Complete</span>
+            </div>
+            {project.tasks.map((task, index) => {
+              const taskBgColor = index % 2 === 0 ? "bg-gray-50" : "bg-white";
+              return (
+                <div
+                  key={task.task_id}
+                  className="contents group"
+                  onMouseEnter={() => setHoveredTaskId(task.task_id)}
+                  onMouseLeave={() => setHoveredTaskId(null)}
+                >
+                  <span className={`p-2 ${taskBgColor} group-hover:bg-blue-50`}>
+                    {task.task_number}
+                  </span>
+                  <div
+                    className={`relative p-2 ${taskBgColor} group-hover:bg-blue-50`}
+                  >
+                    <button
+                      onClick={() =>
+                        handleEditClick(
+                          task.task_id,
+                          task.task_name,
+                          task.task_number
+                        )
+                      }
+                      className={`absolute left-2 px-3 py-1 text-sm font-medium text-white bg-blue-600 rounded hover:bg-blue-700 transition-opacity duration-200 ${
+                        hoveredTaskId === task.task_id
+                          ? "opacity-100"
+                          : "opacity-0"
+                      } ${!canEditFinancials ? "hidden" : ""}`}
+                    >
+                      Edit
+                    </button>
+
+                    <span>{task.task_name}</span>
+                  </div>
+                  <div
+                    className={`relative p-2 ${taskBgColor} group-hover:bg-blue-50 flex justify-center items-center`}
+                  >
+                    {task.costing_complete ? (
+                      <FiCheck className="text-green-500" size={20} />
+                    ) : (
+                      <FiX className="text-red-500" size={20} />
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 CompletedProjectCard.propTypes = {
-  project: PropTypes.object,
-  setIsFinancialsInputModalOpen: PropTypes.func,
-  setSelectedTask: PropTypes.func,
+  project: PropTypes.object.isRequired,
+  setIsFinancialsInputModalOpen: PropTypes.func.isRequired,
+  setSelectedTask: PropTypes.func.isRequired,
+  bgColor: PropTypes.string,
 };
 
 export default CompletedProjectCard;
